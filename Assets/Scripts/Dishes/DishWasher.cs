52b0d40959dde080ca4bf6bb4049ee0e87cf34af
tiiimezombie <tiiimezombie@gmail.com>
2026-05-10 13:41:14 -0400

    Basic pass of minigame - is now playable

--- Assets/Scripts/Dishes/DishWasher.cs
+++ Assets/Scripts/Dishes/DishWasher.cs
@@ -7,8 +7,10 @@ namespace Dishes
     {
         public DishSpawnerManager DishSpawnMgr;
         public WashingSink DirtySinkMgr;
+        public RinsingSink CleanSinkMgr;
+        public TMPro.TextMeshProUGUI ObjectiveText;
 
-        public bool ManagingDish => DirtySinkMgr.PreppingDish;
+        public bool ManagingDish => DirtySinkMgr.PreppingDish || CleanSinkMgr.PreppingDish;
 
         //public bool HoldingDish { get => HeldDish != Dish.DishType.None; }
         //public Dish.DishType HeldDish;
@@ -17,6 +19,7 @@ namespace Dishes
         void Start()
         {
             //HeldDish = Dish.DishType.None;
+            ResolveObjective();
         }
 
         // Update is called once per frame
@@ -35,6 +38,42 @@ namespace Dishes
             {
                 Debug.Log("placeholder cleaning!!");
                 DirtySinkMgr.TestCleaningAttempt();
+            }
+        }
+
+        public void ResolveObjective()
+        {
+            if (CleanSinkMgr.NeedToDryDish)
+            {
+                ObjectiveText.text = "Move Dish To Rack";
+            }
+            else if (CleanSinkMgr.NeedToRinseDish)
+            {
+                ObjectiveText.text = "Rinse Dish";
+            }
+            else if (CleanSinkMgr.AcceptingDishes && DirtySinkMgr.IsDishClean())
+            {
+                ObjectiveText.text = "Prep Dish To Rinse";
+            }
+            else if (DirtySinkMgr.PreppingDish && !DirtySinkMgr.IsDishClean())
+            {
+                ObjectiveText.text = "Wash Dish (C)";
+            }
+            else if (!DirtySinkMgr.HasSoap)
+            {
+                ObjectiveText.text = "Add Soap";
+            }
+            else if (!DirtySinkMgr.PreppingDish && !DirtySinkMgr.NeedsDish)
+            {
+                ObjectiveText.text = "Grab Dish From Water";
+            }
+            else if (DirtySinkMgr.NeedsDish)
+            {
+                ObjectiveText.text = "Soak Dish";
+            }
+            else
+            {
+                ObjectiveText.text = "Wash Dishes!!";
             }
         }
 
